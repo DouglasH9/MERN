547d34a61f277eec68339fc922d3a8d04d34e80e
Doug Herman <douglash9882@gmail.com>
2021-10-25 22:46:00 -0500

    Ninja assignment

---
+++ convert2arrow.js
@@ -0,0 +1,13 @@
+document.getElementById("button").onclick = () => setBackgroundColorById("paragraph" , "blue");
+
+document.getElementById("alert").onclick = () => alert(document.getElementById("popup-input").value);
+
+document.getElementById("hover-this").onmouseover = () => setBackgroundColorById("body" , "white");
+
+document.getElementById("hover-this").onmouseout = () => setBackgroundColorById("body","white");
+
+const getValueFromId = id => document.getElementById(id).value;
+
+const setBackgroundColorById = (id, color) => document.getElementById(id).style = "background-color: " + color;
+
+const mouseOverFunction = el => el.style = "background-color: black";
