62402abe813c2e3b97d5409714388e5dfdc5eabe
Doug Herman <douglash9882@gmail.com>
2021-11-02 00:39:12 -0500

    tabs assignment

---
+++ tabs/src/Components/DisplayContent.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from "react";
+
+const TabRow = (props) => {
+    const onClickHandler = (e) => {
+        e.preventDefault();
+        // some code
+    };
+
+    return (
+        <>
+        <div>
+            
+        </div>
+        </>
+    )
+}
