5c90852911d47bb243f7e5ee8ed4d80cc1d9118e
Doug Herman <douglash9882@gmail.com>
2021-11-09 01:01:48 -0600

    added redirect after delete

--- productmanager/client/src/Views/ProductDetail.js
+++ productmanager/client/src/Views/ProductDetail.js
@@ -1,18 +1,30 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
-import {useParams, Link} from "react-router-dom";
+import {useParams, Link, Redirect} from "react-router-dom";
 
 const ProductDetail = (props) => {
     const [product, setProduct] = useState({})
+    const [isDeleted, setIsDeleted] = useState(false)
     const {id} = useParams();
 
+    
     useEffect(() => {
         axios.get("http://localhost:8000/api/product/" +id)
-            .then(res => setProduct(res.data))
-            .catch(err => console.log(err));
+        .then(res => setProduct(res.data))
+        .catch(err => console.log(err));
     }, [id])
+    
+    const deleteProduct = () => {
+        axios.delete("http://localhost:8000/api/product/" + id)
+            .then(res => {
+                setIsDeleted(true)
+            })
+            .catch(err => console.log(err));
+    }
 
-    return (
+    return isDeleted ?
+    <Redirect to="/api/products"/> :
+    (
         <div>
             <p>Title: {product.title}</p>
             <p>Price: {product.price}</p>
@@ -20,6 +32,7 @@ const ProductDetail = (props) => {
             <Link to={product._id +"/edit"}>
                 Edit
             </Link>
+            <button onClick={deleteProduct}>Delete</button>
         </div>
     )
 }
